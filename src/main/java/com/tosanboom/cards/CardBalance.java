ea336811b314beab67b2832d183f5683c12560b1
Ali Dehghani <ali.dehghani.g@gmail.com>
2017-04-26 15:57:27 +0430

    Added some house keepings material for get balance method

--- src/main/java/com/tosanboom/cards/CardBalance.java
+++ src/main/java/com/tosanboom/cards/CardBalance.java
@@ -2,34 +2,15 @@ package com.tosanboom.cards;
 
 import java.math.BigDecimal;
 
+/**
+ * Represents the balance of a card
+ *
+ * @author Ali Dehghani
+ */
 public class CardBalance {
-    BigDecimal availableBalance;
-    BigDecimal ledgerBalance;
-    String currency;
-
-    public BigDecimal getAvailableBalance() {
-        return availableBalance;
-    }
-
-    public void setAvailableBalance(BigDecimal availableBalance) {
-        this.availableBalance = availableBalance;
-    }
-
-    public BigDecimal getLedgerBalance() {
-        return ledgerBalance;
-    }
-
-    public void setLedgerBalance(BigDecimal ledgerBalance) {
-        this.ledgerBalance = ledgerBalance;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
+    public BigDecimal availableBalance;
+    public BigDecimal ledgerBalance;
+    public String currency;
 
     @Override
     public String toString() {
